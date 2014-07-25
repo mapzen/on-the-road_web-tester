5930fdbdde6a25c216a71b3bce8d9329a8b16d79
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-07-25 13:53:37 -0400

    currentLeg access fix

--- java/src/main/java/com/mapzen/TestRoute.java
+++ java/src/main/java/com/mapzen/TestRoute.java
@@ -5,18 +5,12 @@ import com.mapzen.osrm.Route;
 import org.json.JSONObject;
 
 public class TestRoute extends Route {
-    private int currentLeg;
-
     public TestRoute(String jsonString) {
         super(jsonString);
     }
 
     public TestRoute(JSONObject jsonObject) {
         super(jsonObject);
-    }
-
-    public int getCurrentLeg() {
-        return currentLeg;
     }
 
     public void setCurrentLeg(int currentLeg) {
