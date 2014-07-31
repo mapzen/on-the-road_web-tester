c09f3b75166da21e60a4cd8994d44789d1bf4cfb
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-07-31 11:43:10 -0400

    massive cleanup and making it less silly

--- node/static/app.js
+++ node/static/app.js
@@ -1,97 +1,98 @@
-var dropDot = function(theMap, lat, lng, color, extraInfo) {
-    L.marker([lat, lng], {
-        icon: L.divIcon({
-            className: 'count-icon-' + color,
-            html: "",
-            iconSize: [10, 10]
-        })
-    }).on('mouseover', function(e) {
-        if (extraInfo) {
-            var popup = L.popup().setLatLng(e.latlng) 
-                .setContent("location: " + e.latlng.toString() 
-                             + "speed: " + extraInfo.speed 
-                             + ", bearing: " + extraInfo.bearing)
-                .openOn(theMap);
-        }
-    }).addTo(theMap);
-}
-
-var index = 0;
-var addDot = function(theMap) {
-    if (index < window.locs.length) {
-        var value = window.locs[index];
-        dropDot(theMap, value.corrected_lat, value.corrected_lng, 'red');
-        dropDot(theMap, value.lat, value.lng, 'blue');
-        console.log("Testing");
-        console.log(index);
-        ++index;
-        window.setTimeout(function() {
-            addDot(theMap)
-        }, 200);
-        theMap.setView([value.corrected_lat, value.corrected_lng], 18);
-    }
-}
-
-$(function() {
-    var initialLocation;
-    if (window.locs.length) {
-        initialLocation = new L.LatLng(window.locs[0].lat, window.locs[0].lng);
-    } else {
-        initialLocation = new L.LatLng(window.linePoints[0][0], window.linePoints[0][1]);
-    }
-
-    var map = L.mapbox.map('map', 'baldurg.j052a6a4')
-        .setView([initialLocation.lat, initialLocation.lng], 18);
-    var polyline = L.polyline([], {
-        color: '#000'
-    }).addTo(map);
-    $.each(linePoints, function(index, value) {
-        polyline.addLatLng(new L.LatLng(value[0], value[1]));
-    });
-
-    if (/slowmo/.test(location.search)) {
-        addDot(map);
-    } else {
-        $.each(window.locs, function(index, value) {
-            dropDot(map, value.corrected_lat, value.corrected_lng, 'red');
-            dropDot(map, value.lat, value.lng, 'blue', {bearing: value.bearing, speed: value.speed});
-        })
-    }
-
-
-    var dropper = L.marker(initialLocation, {
-        icon: L.mapbox.marker.icon({
-            'marker-color': 'CC0033'
-        }),
-        draggable: true
-    });
-    var result = L.marker(initialLocation, {
-        icon: L.divIcon({
-            className: 'count-icon-corrector',
-            html: "",
-            iconSize: [10, 10]
-        })
-    }).addTo(map);
-
-    var currentLeg = 0;
-    var lat, lng;
-    dropper.on('dragend', function(x) {
+var MAPZEN = {
+    map: null,
+    resultDot: null,
+    getCorrectedDot: function(x) {
+        var currentLeg = 0;
+        var lat, lng;
         var latLng = x.target.getLatLng();
         var newLatLng = new L.LatLng(latLng.lat, latLng.lng);
-        $.get('snap?current_leg=' + currentLeg + '&route_id=' + $('#map').data().currentRoute + '&lat=' + latLng.lat + '&lng=' + latLng.lng, function(resp) {
-            console.log("||||" + currentLeg + "||||");
+        var params = $.param({
+            current_leg: currentLeg,
+            route_id: $('#map').data().currentRoute,
+            lat: latLng.lat,
+            lng: latLng.lng
+        });
+
+        $.get('snap?' + params, function(resp) {
             responseArray = resp.split(",");
-            console.log(resp);
             if (responseArray.length > 2) {
                 currentLeg = responseArray[2];
-                lat = responseArray[0];
-                lng = responseArray[1];
-                result.setLatLng(new L.LatLng(lat, lng));
-                console.log(newLatLng.distanceTo(new L.LatLng(lat, lng)));
+                MAPZEN.resultDot.setLatLng(new L.LatLng(responseArray[0], responseArray[1]));
             } else {
                 console.log("LOST");
             }
         });
-    });
-    dropper.addTo(map);
+    },
+    addLocations: function(index, value) {
+        L.marker([value.corrected_lat, value.corrected_lng], {
+            icon: L.divIcon({
+                className: 'count-icon-' + 'red',
+                html: "",
+                iconSize: [10, 10]
+            })
+        }).addTo(MAPZEN.map);
+        L.marker([value.lat, value.lng], {
+            icon: L.divIcon({
+                className: 'count-icon-' + 'blue',
+                html: "",
+                iconSize: [10, 10]
+            })
+        }).on('mouseover', function(e) {
+            var popup = L.popup().setLatLng(e.latlng) 
+                .setContent("location: " + e.latlng.toString() 
+                             + "speed: " + value.speed 
+                             + ", bearing: " + value.bearing)
+                .openOn(MAPZEN.map);
+        }).addTo(MAPZEN.map);
+    },
+    initialLocation: null,
+    init: function() {
+        if (window.locs.length) {
+            MAPZEN.initialLocation = new L.LatLng(window.locs[0].lat, window.locs[0].lng);
+        } else {
+            MAPZEN.initialLocation = new L.LatLng(window.linePoints[0][0], window.linePoints[0][1]);
+        }
+
+        MAPZEN.map = L.mapbox.map('map', 'baldurg.j052a6a4')
+            .setView([MAPZEN.initialLocation.lat, MAPZEN.initialLocation.lng], 18);
+    },
+    addRouteLine: function() {
+        var polyline = L.polyline([], {
+            color: '#000'
+        }).addTo(MAPZEN.map);
+
+        $.each(linePoints, function(index, value) {
+            polyline.addLatLng(new L.LatLng(value[0], value[1]));
+        });
+    },
+    populateDots: function() {
+        $.each(window.locs, MAPZEN.addLocations);
+    },
+    addDropper: function() {
+        var dropper = L.marker(MAPZEN.initialLocation, {
+            icon: L.mapbox.marker.icon({
+                'marker-color': 'CC0033'
+            }),
+            draggable: true
+        });
+
+        MAPZEN.resultDot = L.marker(MAPZEN.initialLocation, {
+            zIndexOffset: 1000,
+            icon: L.divIcon({
+                className: 'count-icon-corrector',
+                html: "",
+                iconSize: [10, 10]
+            })
+        }).addTo(MAPZEN.map);
+
+        dropper.on('dragend', MAPZEN.getCorrectedDot);
+        dropper.addTo(MAPZEN.map);
+    }
+};
+
+$(function() {
+    MAPZEN.init();
+    MAPZEN.addRouteLine();
+    MAPZEN.populateDots();
+    MAPZEN.addDropper();
 });
